ee98d5ae051bed718a70958c3843f2998dd37071
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-06-04 21:42:46 -0700

    Add files via upload

---
+++ replace-dot.js
@@ -0,0 +1,8 @@
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+//solutions
+var replaceDots = function(str) {
+    return str.replace(/\./g,'-');
+  }
