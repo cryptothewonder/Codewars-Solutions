e3e95ce98b4cbd99eb31da4659c419a265ad7db4
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-06-27 21:23:04 -0700

    Add files via upload

---
+++ truthy-falsy.js
@@ -0,0 +1,5 @@
+//You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+
+//solutions
+const truthy = [5,6,9,8,7];
+const falsy = [-0, false,"", NaN, null];
