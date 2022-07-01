d690abc70a530c54db5ef20d75aa4fd482fa1c02
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-07-01 16:18:49 -0700

    Add files via upload

---
+++ two-sum.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+//solution
+function twoSum(numbers, target) {
+    for (var i = 0; i < numbers.length-1; i++) {
+        for (var j = i+1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i, j];
+        }
+    }
+}
