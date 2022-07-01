74aa3636a460094cc58e27a663d65d2fb8111dfd
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-06-30 21:46:51 -0700

    Add files via upload

---
+++ simple-bead-count.js
@@ -0,0 +1,7 @@
+//Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// If there are less than 2 blue beads return 0.
+//solutions
+function countRedBeads(n) {
+    if (n < 2) {return 0}
+     else return n*2-2//your code here
+   }
