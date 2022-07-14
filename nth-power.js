d442f9ea895059d670dc636bb30d19876f1e7912
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-07-14 16:11:27 -0700

    Add files via upload

---
+++ nth-power.js
@@ -0,0 +1,15 @@
+// This kata is from check py.checkio.org
+
+// You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+
+//solutions
+function index(array, n){
+    if (n>array.length-1){return -1;}
+   else {return Math.pow(array[n],n);}
+  }
+
+  function index(array, n){
+    //your code here
+    return array[n] ** n || -1;
+  }
