522d8d204fcaea1191da94d3e5a1dfc6206c9d7b
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-07-21 11:34:36 -0700

    Add files via upload

---
+++ reverse-list.js
@@ -0,0 +1,9 @@
+// Write reverseList function that simply reverses lists.
+
+//solutions
+
+function reverseList(arr) {
+    return arr.reverse();
+  }
+
+  const reverseList = arr => [...arr].reverse();
