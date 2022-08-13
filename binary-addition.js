ca8dbb529389974044fa160136807bb2c824d93f
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-08-12 22:42:26 -0700

    Add files via upload

---
+++ binary-addition.js
@@ -0,0 +1,13 @@
+//Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+//solutions
+function addBinary(a,b){
+    let c = a + b;
+    return c.toString(2);
+  }
+
+  nction addBinary(a,b){
+    return (a+b).toString(2)
+  }
