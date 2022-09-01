9cfcee0a2f92ef036c44001617a69ce2c1d432a2
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2022-08-31 23:25:35 -0700

    Add files via upload

---
+++ sort-and-star.js
@@ -0,0 +1,16 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+//solutions
+
+function twoSort(s) {
+    let ans = s.sort()
+    return ans[0].split('').join('***')
+}
+
+function twoSort(s) {
+    return s.sort()[0].split('').join('***');
+  }
