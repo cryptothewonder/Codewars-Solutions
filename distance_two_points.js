becb2918db654de6c3c2f12843bbbde4a727535c
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2023-02-06 23:53:39 -0800

    Add files via upload

---
+++ distance_two_points.js
@@ -0,0 +1,16 @@
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have attributes x and y.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests compare expected result and actual answer with tolerance of 1e-6.
+
+//solutions
+function distanceBetweenPoints(a, b) {
+ return Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y), 2))
+}
+
+function distanceBetweenPoints(a, b) {
+  return Math.hypot(a.x - b.x, a.y - b.y);
+}
