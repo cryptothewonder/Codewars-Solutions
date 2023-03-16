b4fec75d872fdeb095e75ae5ec9f2ecfbcfa0889
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2023-03-16 12:15:49 -0700

    Add files via upload

---
+++ valid_paranthesis.js
@@ -0,0 +1,21 @@
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+//solutions
+function validParentheses(parenStr) {
+    let ans = parenStr.split('')
+    let countLeft = 0
+    
+      for(let i = 0; i<ans.length;i++){
+        if(ans[i] ==='('){
+          countLeft += 1
+        } else {
+          countLeft -=1
+        }
+        if (countLeft<0){
+          return false
+        }
+      
+    }
+  return countLeft === 0
+  }
+  
