113e863d3096b730cc335fd58eb310ea87da42b0
cryptothewonder <100801522+cryptothewonder@users.noreply.github.com>
2023-06-08 10:31:40 -0700

    Add files via upload

---
+++ concat_string_join.js
@@ -0,0 +1,8 @@
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+//solutions
+function splitAndMerge(str, sp) {
+    return str.split(" ").map(word => word.split("").join(sp)).join(" ");
+  }
